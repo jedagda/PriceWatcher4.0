a19907a097bf9fc7433047f6f065e76368e68189
Josh Dagda <jedagda@gmail.com>
2018-11-24 22:39:21 -0700

    Added Icons for the menu items Added and removed icons Cleaned up the code Got rid of unnecessary and redundant methods Cleaned up the action listeners

--- src/ConfigureUI.java
+++ src/ConfigureUI.java
@@ -1,72 +1,102 @@
 import resources.Resources;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.IOException;
-import java.net.URL;
-
-
-
 
 public class ConfigureUI extends JFrame {
 
     private static ConfigureUI configureUI;
-
+    /** The main frame of Price Watcher 4.0 */
     private JFrame mainFrame;
-
+    /** The menu bar */
     private JMenuBar menuBar;
-    private JMenu file;
-    private JMenu help;
-    private JMenuItem exit;
+    /** A menu */
+    private JMenu menu;
+
+    /**
+     * List of menu actions of the menu bar
+     * */
     private JMenuItem add;
+    private JMenuItem check;
     private JMenuItem about;
-    private JPanel itemBoard = new JPanel();
-    private final static String IMAGE_DIR = "/icons/";
+    private JMenuItem exit;
 
-    /** Message bar to display various messages. */
-    //private JLabel msgBar = new JLabel(" ");
+    /**
+     * JPanel where items will be displayed on
+     * */
+    private JPanel itemBoard;
 
+    /**
+     *  Control panel of Price Watcher
+     * */
     private JPanel controlPanel;
+
+    /**
+     * Control Panel action buttons
+     * */
     private JButton checkPrice;
     private JButton addItem;
 
-    private JLabel headerLabel;
+    /**
+     * Messages will be displayed
+     * */
     private JLabel msgBar;
 
-    private Resources image;
+    /**
+     *  Calls the Resources class
+     *  */
+    private Resources resources;
 
-
-    private ConfigureUI(){
-        prepareGUI();
-        showMessage("Welcome");
-        mainFrame.setVisible(true);
-    }
+    /**
+     *  An Icon image
+     * */
+    private Icon icon;
 
     public static void main(String[] args){
         configureUI = new ConfigureUI();
 
     }
 
+    /**
+     * Main Constructor of ConfigureUI class
+     * */
+    private ConfigureUI(){
+        prepareGUI();
+        showMessage("Welcome to Price Watcher");
+        mainFrame.setVisible(true);
+    }
+
+    /**
+     *  Prepares the GUI's layout
+     * */
+    private void prepareGUI(){
+        setMainFrame();
+        setCloser();
+        setItemBoard();
+        setStatusLabel();
+
+        addControlPanelElements();
+        addMenuBarElements();
+        addMainFrameElements();
+
+        mainFrame.setJMenuBar(menuBar);
+    }
+    /**
+     * Set's the main frame's title, dimensions, and layout
+     * */
     private void setMainFrame(){
-        mainFrame = new JFrame("Price Watecher 4.0");
+        mainFrame = new JFrame("Price Watcher 4.0");
         mainFrame.setSize(400, 600);
         mainFrame.setLayout(new GridLayout(4,1));
     }
 
-    private void setHeaderLabel(){
-        headerLabel = new JLabel("Sample Header", JLabel.CENTER );
-    }
-
-    private void setStatusLabel(){
-        msgBar = new JLabel("",JLabel.CENTER);
-        msgBar.setSize(350,50);
-    }
-
+    /**
+     * Sets the closer on the window
+     * */
     private void setCloser(){
         mainFrame.addWindowListener(new WindowAdapter() {
             @Override
@@ -75,91 +105,100 @@ public class ConfigureUI extends JFrame {
             }
         });
     }
-
-    private void setControlPanel(){
-        controlPanel = new JPanel();
-        controlPanel.setLayout(new FlowLayout());
-    }
-
-    private void setMenuBar(){
-        menuBar = new JMenuBar();
-    }
-
+    /**
+     *  Sets the item board from where the items will be displayed
+     */
     private void setItemBoard(){
+        itemBoard = new JPanel();
         itemBoard.setBorder(BorderFactory.createCompoundBorder(
                 BorderFactory.createEmptyBorder(10,16,0,16),
                 BorderFactory.createLineBorder(Color.GRAY)));
-        itemBoard.setSize(mainFrame.getMinimumSize());
+
         itemBoard.setLayout(new GridLayout(1,1));
-    }
-    //push
-    private void addMenuBarElements(){
-        file = new JMenu();
-        Icon icon = createImageIcon("icons/menu.png", "Menu");
-        file.setIcon(icon);
-        help = new JMenu("Help");
-        exit = new JMenuItem("Exit");
-        exit.addActionListener(new ExitListener());
-        about = new JMenuItem("About");
-        add = new JMenuItem("Add");
-
-        menuBar.add(file);
-        menuBar.add(help);
-
-        file.add(add);
-        file.add(exit);
-
-        help.add(about);
-
+        itemBoard.setSize(mainFrame.getMinimumSize());
     }
 
-    protected ImageIcon createImageIcon (String path, String description){
-        java.net.URL imgURL = getClass().getResource(path);
-        if (imgURL != null) {
-            return new ImageIcon (imgURL, description);
-        } else {
-            System.err.println("Could't find file " + path);
-            return null;
-        }
+    /**
+     * Sets the Status label which indicates what actions the program has taken
+     * */
+    private void setStatusLabel(){
+        msgBar = new JLabel("",JLabel.CENTER);
+        msgBar.setSize(350,50);
     }
 
+    /**
+     * Adds the elements to the main frame
+     */
     private void addMainFrameElements(){
- //       mainFrame.add(headerLabel);
         mainFrame.add(controlPanel);
         mainFrame.add(itemBoard);
         mainFrame.add(msgBar);
     }
 
+    /**
+     * Adds the the menu items to the menu  and add the menus to the menu bar
+     * */
+    private void addMenuBarElements(){
+        menuBar = new JMenuBar();
+        resources = new Resources();
+
+        menu = new JMenu();
+        icon = resources.createImageIcon("menu.png", "Menu");
+        menu.setIcon(icon);
+
+        add = new JMenuItem("Add");
+        icon = resources.createImageIcon("add.png", "Add Item");
+        add.setIcon(icon);
+        add.setActionCommand("Add");
+        add.addActionListener(new Listener());
+
+        check = new JMenuItem("Check");
+        icon = resources.createImageIcon("check.png" , "Check Price");
+        check.setIcon(icon);
+        check.setActionCommand("Check");
+        check.addActionListener(new Listener());
+
+        about = new JMenuItem("About");
+        icon = resources.createImageIcon("about.png" , "About");
+        about.setIcon(icon);
+        about.setActionCommand("About");
+        about.addActionListener(new Listener());
+
+
+        exit = new JMenuItem("Exit");
+        icon = resources.createImageIcon("exit.png", "Exit");
+        exit.setIcon(icon);
+        exit.addActionListener(new Listener());
+
+        menuBar.add(menu);
+        menu.add(add);
+        menu.add(check);
+        menu.add(about);
+        menu.add(exit);
+
+    }
+
+    /**
+     * Adds the elements to the control panel
+     * */
     private void addControlPanelElements(){
+
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new FlowLayout());
+
         checkPrice = new JButton("Check");
         addItem = new JButton("Add");
         checkPrice.setActionCommand("Check");
         addItem.setActionCommand("Add");
-        checkPrice.addActionListener(new CheckButtonListener());
-        addItem.addActionListener(new AddItemButtonListener());
+        checkPrice.addActionListener(new Listener());
+        addItem.addActionListener(new Listener());
         controlPanel.add(checkPrice);
         controlPanel.add(addItem);
     }
 
-
-
-    private void prepareGUI(){
-        setMainFrame();
-        setMenuBar();
-        setHeaderLabel();
-        setStatusLabel();
-        setCloser();
-        setControlPanel();
-        setItemBoard();
-        addMainFrameElements();
-        addControlPanelElements();
-        addMenuBarElements();
-        mainFrame.setJMenuBar(menuBar);
-
-
-    }
-
-    /** Show briefly the given string in the message bar. */
+    /** Show briefly the given string in the message bar.
+     *  Code by: Yoonsik Cheon
+     *  */
     private void showMessage(String msg) {
         msgBar.setText(msg);
         new Thread(() -> {
@@ -173,43 +212,52 @@ public class ConfigureUI extends JFrame {
         }).start();
     }
 
-    private class CheckButtonListener implements ActionListener{
+    /**
+     * Adds and Item
+     */
+    private void addItem(){
+        showMessage("Item Added");
+    }
+
+    /**
+     * Checks the price of the items added
+     */
+    private void checkPrice(){
+        showMessage("Price Checked");
+    }
+
+    /**
+     * Displays About the program
+     */
+    private void about(){
+        showMessage("Display About");
+    }
+
+    /**
+     * Exits the program with exit code 0
+     */
+    private void exit(){
+        System.exit(0);
+    }
+
+    /**
+     *Listener for program
+     * */
+    private class Listener implements ActionListener{
         public void actionPerformed(ActionEvent actionEvent) {
             String command = actionEvent.getActionCommand();
-            if (command.equals("Check")) {
-               showMessage("Price Checked");
+            if(command.equals("Add")){
+                addItem();
+            } else if (command.equals("Check")){
+                checkPrice();
+            } else if (command.equals("About")){
+                about();
+            } else if (command.equals("Exit")){
+                exit();
             }
+
         }
     }
-
-    private class AddItemButtonListener implements ActionListener{
-        public void actionPerformed(ActionEvent actionEvent){
-            String command = actionEvent.getActionCommand();
-            if(command.equals("Add")) {
-                showMessage("Item Added");
-            }
-        }
-    }
-
-    private class ExitListener implements ActionListener{
-        public void actionPerformed(ActionEvent actionEvent) {
-            String command = actionEvent.getActionCommand();
-            if (command.equals("Exit")) {
-                System.exit(0);
-            }
-        }
-    }
-
-    public Image getImage(String file) {
-        try {
-            URL url = new URL(getClass().getResource(IMAGE_DIR), file);
-            return ImageIO.read(url);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
 
 
 }
--- src/resources/Resources.java
+++ src/resources/Resources.java
@@ -1,18 +1,21 @@
 package resources;
 
 import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
 
 public class Resources {
 
-    private Resources(){
+    public Resources(){
 
     }
 
     /** Directory for image files: src/image in Eclipse. */
+    private final static String ICONS_DIR = "/icons/";
     private final static String IMAGE_DIR = "/icons/";
+
 
     /** Return the image stored in the given file. */
     public BufferedImage getImage(String file) {
@@ -23,5 +26,15 @@ public class Resources {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public ImageIcon createImageIcon(String path, String description){
+        java.net.URL imgURL = getClass().getResource(ICONS_DIR + path);
+        if (imgURL != null) {
+            return new ImageIcon (imgURL, description);
+        } else {
+            System.err.println("Could't find file " + path);
+            return null;
+        }
     }
 }
