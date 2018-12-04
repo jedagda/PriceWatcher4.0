a1a5a4698b2c55c5aa2b49240bcbdee624f46a3c
eris <jedagda@gmail.com>
2018-12-03 23:11:33 -0700

    Modified project into a Maven project to be able to use JSON and GSON seamlessly.

--- src/main/java/resources/Resources.java
+++ src/main/java/resources/Resources.java
@@ -3,24 +3,30 @@ package resources;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
+import java.io.*;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.security.CodeSource;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class Resources {
+
+    static Class source = Resources.class;
 
     public Resources(){
 
     }
 
     /** Directory for image files: src/image in Eclipse. */
-    private final static String ICONS_DIR = "/icons/";
-    private final static String IMAGE_DIR = "/icons/";
+    private final static String ICONS_DIR = "/";
+    private final static String IMAGE_DIR = "/";
 
 
     /** Return the image stored in the given file. */
     public BufferedImage getImage(String file) {
         try {
-            URL url = new URL(getClass().getResource(IMAGE_DIR), file);
+            URL url = new URL(getClass().getResource(ICONS_DIR), file);
             return ImageIO.read(url);
         } catch (IOException e) {
             e.printStackTrace();
@@ -29,7 +35,7 @@ public class Resources {
     }
 
     public ImageIcon createImageIcon(String path, String description){
-        java.net.URL imgURL = getClass().getResource(ICONS_DIR + path+".png");
+        java.net.URL imgURL = getClass().getResource(IMAGE_DIR + path+".png");
         if (imgURL != null) {
             return new ImageIcon (imgURL, description);
         } else {
@@ -37,4 +43,6 @@ public class Resources {
             return null;
         }
     }
+
+
 }
